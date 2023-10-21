f7f813124b484855c9e60d598c2e8b8fd7899589
Phoshoko Thabang <phoshokothabang1@gmail.com>
2023-10-21 11:52:35 +0200

    Added the paragraph tag and styled it red

---
+++ script.js
@@ -0,0 +1,8 @@
+const container = document.querySelector('#container');
+
+const paragraph = document.createElement('p');
+paragraph.textContent = "Hey I'm red";
+paragraph.style.color = 'red';
+
+container.appendChild(paragraph);
+
